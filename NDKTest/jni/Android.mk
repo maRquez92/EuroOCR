e316073157adb9e765014cd5ac7e515012c01e65
maRquez92 <paulinhobighit@hotmail.com>
2014-05-19 19:35:33 +0100

    Build With OpenCV and Nonfree libs

--- NDKTest/jni/Android.mk
+++ NDKTest/jni/Android.mk
@@ -3,7 +3,30 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
  
 # Here we give our module name and source file(s)
+
 LOCAL_MODULE    := ndkfoo
 LOCAL_SRC_FILES := ndkfoo.cpp
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := nonfree_prebuilt
+LOCAL_SRC_FILES := libnonfree.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := opencv_java_prebuilt
+LOCAL_SRC_FILES := libopencv_java.so
+include $(PREBUILT_SHARED_LIBRARY)
  
+include $(CLEAR_VARS)
+ 
+# Here we give our module name and source file(s)
+LOCAL_C_INCLUDES:= C:/Users/Paulo/Downloads/OpenCV-2.4.8-android-sdk/OpenCV-2.4.8-android-sdk/sdk/native/jni/include
+
+LOCAL_MODULE    := ndkCV
+LOCAL_SHARED_LIBRARIES := nonfree_prebuilt opencv_java_prebuilt
+
+LOCAL_SRC_FILES := ndkCV.cpp
+
 include $(BUILD_SHARED_LIBRARY)
